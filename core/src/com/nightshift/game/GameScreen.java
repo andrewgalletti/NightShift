85c91d4edcd0a1f805d094723a059da1ee262d86
Mark Coretsopoulos <mcoretso@macalester.edu>
2017-04-14 11:26:03 -0500

    Added spiralmap

--- core/src/com/nightshift/game/GameScreen.java
+++ core/src/com/nightshift/game/GameScreen.java
@@ -43,7 +43,7 @@ public class GameScreen implements Screen {
         String fileName = "";
         switch(levelIndex) {
             case 0:
-                fileName = "Maps/testmap.tmx";
+                fileName = "Maps/spiralmap.tmx";
                 break;
             case 1:
                 fileName = "Maps/easymap.tmx";

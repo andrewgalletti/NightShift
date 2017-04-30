ef21c00945a5035ddd0a2b10fea2bc13862cc548
Andrew Cui <acui@macalester.edu>
2017-04-30 15:58:57 -0500

    All Caps

--- core/src/com/nightshift/game/screens/SuccessScreen.java
+++ core/src/com/nightshift/game/screens/SuccessScreen.java
@@ -39,7 +39,7 @@ public class SuccessScreen implements Screen{
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        font2.draw(batch, "You have escaped!", 50, Constants.VIEWPORT_HEIGHT/2 + 100);
+        font2.draw(batch, "You Have Escaped!", 50, Constants.VIEWPORT_HEIGHT/2 + 100);
         font1.draw(batch, "Press Space to Restart!", Constants.VIEWPORT_WIDTH/2 - 80, Constants.VIEWPORT_HEIGHT/2 - 50);
         font1.draw(batch, "Q to Quit", Constants.VIEWPORT_WIDTH/2 - 80, Constants.VIEWPORT_HEIGHT/2-100);
         batch.end();

667ea05c60e416029b573962b57d714a0a8505cc
Andrew Galletti <agallett@macalester.edu>
2018-09-28 16:36:06 -0500

    clean constant

--- core/src/com/nightshift/game/data/Constants.java
+++ core/src/com/nightshift/game/data/Constants.java
@@ -8,15 +8,15 @@ public class Constants {
 
     //Used to convert from Box2d units to relative coordinate system.
     public static final float PIXELS_TO_METERS = 100f;
-
-    public static final int VIEWPORT_WIDTH = 500;
     public static final int VIEWPORT_HEIGHT = 500;
+    public static final int VIEWPORT_WIDTH = 500;
 
     //Sound Volumes expressed as percentages of original volume.
     public static final float INTO_PURSUIT_VOLUME = .3f;
     public static final float TAKE_DAMAGE_VOLUME = .5f;
     public static final float GHOST_CHUCKLE_VOLUME = .8f;
     public static final float GAME_MUSIC_VOLUME = .4f;
+    
     //Sets the delay in milliseconds between ghostChuckle.play() and gameMusic.loop()
     public static final float START_SOUND_MUSIC_DELAY = 900;
 }

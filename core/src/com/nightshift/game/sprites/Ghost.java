43bf36705f668cb7f60e1fecd20c0719fe085c25
Andrew Galletti <agallett@macalester.edu>
2018-10-11 11:52:46 -0500

    Preparing my code for pull
    Added a comment and hiddendoor map

--- core/src/com/nightshift/game/sprites/Ghost.java
+++ core/src/com/nightshift/game/sprites/Ghost.java
@@ -28,6 +28,7 @@ public class Ghost {
 
     private Vector2 position = new Vector2(0,0);
     private Vector2 velocity = new Vector2(0,0);
+    //Acceleration manipulation used to give ghost movement curvature.
     private Vector2 acceleration = new Vector2(0,0);
     private Vector2 post = new Vector2(0,0);
 

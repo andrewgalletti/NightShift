2d44783c561297c5dd99e6f12375e767ca790080
Kelli Mandel <kellimandel@gmail.com>
2018-11-05 15:54:36 -0600

    * Added more texture additions to maps * Included rug files

---
+++ core/assets/Maps/Official Maps/rugs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="rugs" tilewidth="32" tileheight="32" tilecount="39" columns="39">
+ <image source="rugs.png" width="1248" height="32"/>
+</tileset>
---
+++ core/assets/Maps/Official Maps/rugs2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="rugs2" tilewidth="16" tileheight="16" tilecount="45" columns="45">
+ <image source="rugs2.png" width="720" height="16"/>
+</tileset>
